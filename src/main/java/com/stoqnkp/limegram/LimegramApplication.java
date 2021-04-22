73fb9589e0379d685bd52ebd033d2449ffe347cc
Stoyan Panayotov <stoqnkp@gmail.com>
2021-04-22 08:31:53 +0300

    init spring boot project with web and websocket dependencies

---
+++ src/main/java/com/stoqnkp/limegram/LimegramApplication.java
@@ -0,0 +1,13 @@
+package com.stoqnkp.limegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimegramApplication.class, args);
+	}
+
+}
